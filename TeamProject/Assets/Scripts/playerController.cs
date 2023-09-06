70952aeda48d82e8254cc953beb86d3a4cd07d0b
Alexis-599 <67765860+Alexis-599@users.noreply.github.com>
2023-09-06 15:15:22 -0400

    Player shoot and takeDamage

--- TeamProject/Assets/Scripts/playerController.cs
+++ TeamProject/Assets/Scripts/playerController.cs
@@ -2,26 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour
+public class playerController : MonoBehaviour, IDamage
 {
     [Header("----- Components -----")]
     [SerializeField] CharacterController controller;
 
     [Header("----- Player Stats -----")]
-    [Range(1, 10)] [SerializeField] int HP;
-    [Range(3, 10)] [SerializeField] float playerSpeed;
+    [Range(1, 10)] [SerializeField] int HP = 10;
+    [Range(3, 10)] [SerializeField] float playerSpeed = 8;
     [Range(1, 10)] [SerializeField] float jumpHeight = 2.5f;
     [Range(-35, -10)] [SerializeField] float gravityValue = -25;
 
-    //[Header("----- Gun Stats -----")]
-    //[SerializeField] float shootRate;
-    //[SerializeField] int shootDamage;
-    //[SerializeField] int shootDistance;
+    [Header("----- Gun Stats -----")]
+    [SerializeField] float shootRate = 2;
+    [SerializeField] int shootDamage = 1;
+    [SerializeField] int shootDistance = 15;
 
     private Vector3 move;
     private Vector3 playerVelocity;
     private bool isGrounded;
     private int jumpedTimes;
+    private bool isShooting;
     int maxJumps = 2;
 
     void Start()
@@ -32,6 +33,9 @@ public class playerController : MonoBehaviour
     void Update()
     {
         Movement();
+
+        if (Input.GetButton("Fire1") && !isShooting)
+            StartCoroutine(shoot());
     }
 
     void Movement()
@@ -62,5 +66,34 @@ public class playerController : MonoBehaviour
         // Add gravity to player's Y velocity and make him move
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
+    }
+
+    public void takeDamage(int damageAmount)
+    {
+        HP -= damageAmount;
+
+        if (HP <= 0)
+        {
+            // you lose
+        }
+    }
+
+    IEnumerator shoot()
+    {
+        isShooting = true;
+
+        RaycastHit hitInfo;
+        Ray ray = Camera.main.ViewportPointToRay(new Vector2(0.5f, 0.5f));
+
+        if (Physics.Raycast(ray, out hitInfo, shootDistance))
+        {
+            IDamage damageable = hitInfo.collider.GetComponent<IDamage>();
+
+            if (damageable != null)
+                damageable.takeDamage(shootDamage);
+        }
+
+        yield return new WaitForSeconds(shootRate);
+        isShooting = false;
     }
 }
