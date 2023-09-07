27bf6d4665d61a868f97c4e2070da1df12d8f240
ArcadeGamer62 <kdoughk@gmail.com>
2023-09-07 10:36:06 -0400

    Tried to make a gunner

---
+++ TeamProject/Assets/Scripts/HeavyGunnerAI.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class HeavyGunnerAI : MonoBehaviour, IDamage, IPhysics
+{
+    [SerializeField] Renderer model;
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Transform shootPos;
+
+
+    [SerializeField] int HP;
+    [SerializeField] int targetFaceSpeed;
+
+    [SerializeField] float shootRate;
+    [SerializeField] GameObject bullet;
+
+    Vector3 playerDirection;
+    bool playerInRange;
+    bool isShooting;
+
+    void Start()
+    {
+        GameManager.instance.updatGameGoal(1);
+    }
+
+    void Update()
+    {
+        if (playerInRange)
+        {
+            playerDirection = GameManager.instance.player.transform.position - transform.position;
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                faceTarget();
+
+                if (!isShooting)
+                {
+                    StartCoroutine(shoot());
+                }
+
+            }
+            if(!isShooting)
+                agent.SetDestination(GameManager.instance.player.transform.position);
+        }
+
+    }
+
+    IEnumerator shoot()
+    {
+        isShooting = true;
+        Instantiate(bullet, shootPos.position, transform.rotation);
+        yield return new WaitForSeconds(shootRate);
+        isShooting = false;
+    }
+
+    public void takeDamage(int amount)
+    {
+        HP -= amount;
+
+        StartCoroutine(FlashDamage());
+
+        if (HP <= 0)
+        {
+            GameManager.instance.updatGameGoal(-1);
+            Destroy(gameObject);
+        }
+
+    }
+    IEnumerator FlashDamage()
+    {
+        Color origColor = model.material.color;
+
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        model.material.color = origColor;
+    }
+
+    void faceTarget()
+    {
+        Quaternion rotation = Quaternion.LookRotation(playerDirection);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * targetFaceSpeed);
+    }
+
+    public void physics(Vector3 direction)
+    {
+        agent.velocity += (direction / 2);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+
+}
