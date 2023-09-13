7e5a4a2ada6d5e6c6a1c7e7f4820a4c74573935e
Christopher Jenkins <gamermasterdudes@gmail.com>
2023-09-13 17:39:28 -0400

    Spawners now spawn only one type of enemy each

---
+++ TeamProject/Assets/Scripts/EnemyHeavySpawner.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] GameObject enemySapwner_Heavy;
+
+    [SerializeField] float spawenerInterval_Heavy = 3.5f;
+
+    void Start()
+    {
+        StartCoroutine(spawnEnemy(spawenerInterval_Heavy, enemySapwner_Heavy));
+    }
+
+    public IEnumerator spawnEnemy(float interval, GameObject enemy)
+    {
+        yield return new WaitForSeconds(interval);
+        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-5f, 5f), Random.Range(-6f, 6f), 0), Quaternion.identity);
+        StartCoroutine(spawnEnemy(interval, enemy));
+    }
+}
