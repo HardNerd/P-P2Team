8f63d69a73212dbd5d2beb6e69acc63bff4ffb85
Macy1123 <AMBurkemper@student.fullsail.edu>
2023-10-10 13:29:56 -0400

    checkpoints change color
    Your welcome for this unneeded but interesting change. You can also change the color to your will, by changing the field.

--- TeamProject/Assets/Scripts/checkpoint.cs
+++ TeamProject/Assets/Scripts/checkpoint.cs
@@ -1,15 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class checkpoint : MonoBehaviour
+public class checkpoint : MonoBehaviour, IDataPersistence
 {
+    [SerializeField] Color color = new Color(255, 215, 0);
+    [SerializeField] private string guid;
+
+    [ContextMenu("Generate guid for ID")]
+    private void GenerateGuid()
+    {
+        guid = System.Guid.NewGuid().ToString();
+    }
+    bool colorChanged;
+    public void LoadData(GameData data)
+    {
+        data.checkPointColorChange.TryGetValue(guid, out colorChanged);
+        if (colorChanged)
+        {
+            gameObject.GetComponentInChildren<Renderer>().material.color = color;
+
+        }
+    }
+
+    public void SaveData(GameData data)
+    {
+        if (data.checkPointColorChange.ContainsKey(guid))
+        {
+            data.checkPointColorChange.Remove(guid);
+        }
+        data.checkPointColorChange.Add(guid, colorChanged);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && GameManager.instance.playerSpawnPOS.transform.position != transform.position)
         {
             GameManager.instance.playerSpawnPOS.transform.position = transform.position;
             StartCoroutine(GameManager.instance.checkpointPopup());
+            gameObject.GetComponentInChildren<Renderer>().material.color = color;
+            colorChanged = true;
             DataPersistenceManager.Instance.SaveGame();
         }
     }
